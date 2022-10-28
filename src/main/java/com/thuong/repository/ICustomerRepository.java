308dee0a89b4fbd74be8f7901653a806a0b9bb38
NguyenThuong-code <you@example.com>
2022-10-28 09:27:15 +0700

    API

---
+++ src/main/java/com/thuong/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.thuong.repository;
+
+import com.thuong.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/thuong/service/impl/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.thuong.service.impl;
+
+import com.thuong.model.Customer;
+import com.thuong.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
